6af80752495c57df3f87fad915e3370dd92cbf5b
ankur <ankurmahapatra90@gmail.com>
2019-09-08 12:22:10 +0530

    Third commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    requireDir = require('require-dir'),
+    config = require('./config/development'),
     path = require('path');
 
 app.use(bodyParser());
@@ -10,7 +13,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'client')));
 
 
+var router = express.Router();
+var routes = require('./server/routes');
+
+routes.register(router);
+app.use('/api', router);
+
+mongoose.connect('mongodb://' + config.db.mongo.host + ':' + config.db.mongo.port + '/' + config.db.mongo.db);
 app.listen(9000, function () {
-    console.log("Welcome to Police_Squad, server started at 3000 :")
+    console.log("Welcome to attendanceManagement, server started at 9000 : "+config.db.mongo.db)
 })
 module.exports = app;
---
+++ server/routes.js
@@ -0,0 +1,19 @@
+var requireDir = require('require-dir');
+/*
+var controllers = requireDir('./controller/api');
+var Common = require('./util/common');
+*/
+
+
+module.exports.register = function (router) {
+    // ADD API
+   /* router.route('/policeOfficer').post(Common.validateRequest,controllers.policeAPI.addPoliceOfficer);
+    router.route('/policeOfficer').get(Common.validateRequest,controllers.policeAPI.getPoliceOfficer);
+
+    router.route('/cases').post(Common.validateRequest,controllers.casesAPI.addCase);
+    router.route('/cases').get(Common.validateRequest,controllers.casesAPI.getCases);
+    router.route('/cases/:id').put(Common.validateRequest,controllers.casesAPI.updateCase);*/
+
+
+
+};
